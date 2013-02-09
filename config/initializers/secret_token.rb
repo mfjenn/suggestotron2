b94008dcd631a498071fc577abb8484b6993e738
Mary Jenn <mary@maryfjenn.com>
2013-02-09 12:03:02 -0800

    changed pg gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Suggestotron::Application.config.secret_token = '96beab107dcedb361819adaa624aa028326b1f6bd96083954a183283402fbdbaecf695fee3f48ee2f2041ed613b1b53acc1942db5c0aad333d24773998b04c81'
